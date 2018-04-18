c852b57e8e0487f78e02cd0d2bccdeb39a7e1586
bobilev <l7upoy@gmail.com>
2018-04-18 18:02:33 +0500

    create chat direction

---
+++ chat/chat.go
@@ -0,0 +1,76 @@
+package chat
+
+import (
+	"github.com/bobilev/golang-chat-bot-vk"
+	"fmt"
+	"github.com/bobilev/chatBOOK/dbwork"
+	"strconv"
+)
+type StatusUser struct {
+	LastStore int
+	LastStep string
+	Answer map[string]string
+}
+func (su *StatusUser) Clear() {
+	for k := range su.Answer {
+		delete(su.Answer,k)
+	}
+}
+func InitStatusUsers() map[int]StatusUser{
+	mapStatusUsers := make(map[int]StatusUser)
+	allUser := dbwork.SelectAllUsers()
+	for id,user := range allUser {
+		var st StatusUser
+		st.LastStore = user.LastStore
+		st.LastStep = user.LastStep
+		mapStatusUsers[id] = st
+	}
+
+}
+func InitChatBot() {
+	accessToken := "b25e0478970ebcde8977b7c7b9b8562e28cce81c9f80518b0fa72196fdc0588d833ff6f298a821d12ba18"
+
+	bot := vkchatbot.InitBot(accessToken)
+	bot.Log = 1 // 0,1,2 - уровни отображения логов
+	updates := bot.StartLongPollServer()
+
+	mapStatusUsers := InitStatusUsers()
+	for update := range updates {
+		//fmt.Println("UserID:",update.UserId,"Text Message:",update.Body)
+		//if update.Body == "hi" {
+		//	res , _ := bot.SendMessage(update.UserId,"Hello")
+		//	fmt.Println("[res]",res.MessageID)
+		//}
+		//if update.Body == "sex" {
+		//	res , _ := bot.SendDoc(update.UserId,"photo",456239017,"секси эльфийка")
+		//	fmt.Println("[res]",res.MessageID)
+		//}
+		if update.Body != "" {
+			//allUser := dbwork.SelectAllUsers()
+			//fmt.Println("allUser",allUser)
+			if _,ok := mapStatusUsers[update.UserId]; ok {
+				fmt.Println("Есть в базе")
+
+
+			} else {
+				dbwork.InsertNewUser(update.UserId,0,"")
+				mapStatusUsers[update.UserId] = StatusUser{0,"",nil}
+				fmt.Println("Нету в базе")
+			}
+			//res , _ := bot.SendMessage(update.UserId,"Hello")
+			//fmt.Println("[res]",res.MessageID)
+		}
+		if update.Body == "00" {
+			mapStores := dbwork.SelectStores()
+			var catalog string
+			i := 1
+			for _, store := range mapStores {
+				catalog += strconv.Itoa(i) +" - "+ store.Text+"\n"
+				i++
+			}
+			res , _ := bot.SendMessage(update.UserId,catalog)
+			fmt.Println("[res]",res.MessageID)
+		}
+
+	}
+}
--- main.go
+++ main.go
@@ -1,53 +1,8 @@
 package main
 
-import (
-	"github.com/bobilev/golang-chat-bot-vk"
-	"github.com/bobilev/chatBOOK/dbwork"
-	"fmt"
-	"strconv"
-)
+import "github.com/bobilev/chatBOOK/chat"
 
 func main() {
-	accessToken := "b25e0478970ebcde8977b7c7b9b8562e28cce81c9f80518b0fa72196fdc0588d833ff6f298a821d12ba18"
-
-	bot := vkchatbot.InitBot(accessToken)
-	bot.Log = 1 // 0,1,2 - уровни отображения логов
-	updates := bot.StartLongPollServer()
-
-	for update := range updates {
-		fmt.Println("UserID:",update.UserId,"Text Message:",update.Body)
-		if update.Body == "hi" {
-			res , _ := bot.SendMessage(update.UserId,"Hello")
-			fmt.Println("[res]",res.MessageID)
-		}
-		if update.Body == "sex" {
-			res , _ := bot.SendDoc(update.UserId,"photo",456239017,"секси эльфийка")
-			fmt.Println("[res]",res.MessageID)
-		}
-		if update.Body == "db" {
-			allUser := dbwork.SelectAllUsers()
-			fmt.Println("allUser",allUser)
-			if _,ok := allUser[update.UserId]; ok {
-				fmt.Println("Есть в базе")
-			} else {
-				dbwork.InsertNewUser(update.UserId,0,"")
-				fmt.Println("Нету в базе")
-			}
-			//res , _ := bot.SendMessage(update.UserId,"Hello")
-			//fmt.Println("[res]",res.MessageID)
-		}
-		if update.Body == "00" {
-			mapStores := dbwork.SelectStores()
-			var catalog string
-			i := 1
-			for _, store := range mapStores {
-				catalog += strconv.Itoa(i) +" - "+ store.Text+"\n"
-				i++
-			}
-			res , _ := bot.SendMessage(update.UserId,catalog)
-			fmt.Println("[res]",res.MessageID)
-		}
-
-	}
+	chat.InitChatBot()
 
 }
